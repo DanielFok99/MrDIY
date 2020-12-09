42b246c1bd1c6712c51482acd6de2df2ad2c2f46
DanielFok99 <43980351+DanielFok99@users.noreply.github.com>
2020-12-10 05:09:00 +0800

    Update 404 page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html class="no-js" lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>404 Error - srtdash</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="shortcut icon" type="image/png" href="{{asset('favicon.ico')}}">
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    <link rel="stylesheet" href="{{asset('css/app2.css')}}">
+
+</head>
+
+<body>
+<!--[if lt IE 8]>
+<p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+<![endif]-->
+<!-- preloader area start -->
+<div id="preloader">
+    <div class="loader"></div>
+</div>
+<!-- preloader area end -->
+<!-- error area start -->
+<div class="error-area ptb--100 text-center">
+    <div class="container">
+        <div class="error-content">
+            <h2>404</h2>
+            <p>Ooops! Something went wrong .</p>
+            <a href="{{route('home')}}">Back to Dashboard</a>
+        </div>
+    </div>
+</div>
+
+<script src="{{asset('js/app.js')}}"></script>
+<script src="{{asset('js/app2.js')}}"></script>
+
+</body>
+
+</html>
